3c594f1c5955dacbb17c2a3ccbbbd18db29245f4
J.D. DeVaughn-Brown <jd@cs.umass.edu>
2019-04-17 16:01:38 -0700

    feat: Add clear database functionality
    resources

---
+++ project/clear_database.py
@@ -0,0 +1,18 @@
+"""Python file to populate database with fake data"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Student, engine, Pref, Project
+
+# ENGINE = create_engine('mysql+pymysql://chadwick:godolphins@apex-matching.c0plu8oomro4.us-east-2.rds.amazonaws.com:3306/testdb')
+ENGINE = create_engine('mysql+pymysql://chadwick:godolphins@apex-matching2.c0plu8oomro4.us-east-2.rds.amazonaws.com:3306/production')
+Base.metadata.bind = ENGINE
+DBSESSION = sessionmaker(bind=ENGINE)
+session = DBSESSION()
+
+num_delete= session.query(Pref).delete()
+print num_delete
+print session.query(Student).delete()
+# session.query(Project).delete()
+
+session.commit()
+
