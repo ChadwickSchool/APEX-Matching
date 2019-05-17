552fb7a32b63396c45bcbedc0ea392f82079ba6e
Dylan <dnguyen2020@chadwickschool.org>
2019-05-17 12:13:59 -0700

    Chore: Cleaned up code a bit.

--- project/test_cases.py
+++ project/test_cases.py
@@ -20,32 +20,8 @@ SESSION = DBSESSION()
 
 
 class BasicTests(unittest.TestCase):
-#     """Umbrella class of all test cases"""
+    """Umbrella class of all test cases"""
 
-    # def test_raw_score(self):
-    #     """Test if get raw score function works"""
-    #     list_score = []
-    #     projects = SESSION.query(Project).all()
-    #     for proj in projects:
-    #         score = get_raw_score(proj)
-    #         list_score.append(score)
-    #     expected_results = [0]
-    #     # print "raw score: "
-    #     # print list_score
-    #     self.assertEqual(list_score, expected_results)
-
-    # def test_pop_score(self):
-    #     """Test if get pop score function works"""
-    #     list_score = []
-    #     projects = SESSION.query(Project).all()
-    #     for proj in projects:
-    #         score = get_popularity_score(proj)
-    #         list_score.append(score)
-    #     expected_results = [0]
-    #     # print "pop score: "
-    #     # print list_score
-    #     self.assertEqual(list_score, expected_results)
-    
     def test_max_pop(self):
         """Test to see if any population is above 15"""
         list_num_fail = 0
@@ -59,28 +35,6 @@ class BasicTests(unittest.TestCase):
             print "Number of Overloaded Projects: " 
             print list_num_fail
             assert False, "Number of overloaded projects: " + str(list_num_fail)
-                
-    
-    def create_random_project(self):
-        SESSION = DBSESSION()
-        test = Project(name='That\'s Cringe: Why Fortnite Means the End of Gen Z', session_number = 2)
-        SESSION.add(test)
-        SESSION.commit()
-    
-    # def test_create_user(self):
-    #     """Test to see if create user works"""
-    #     student_test = Student(name='test', email='test@testycles.com')
-    #     SESSION.add(student_test)
-    #     SESSION.commit()
-    #     is_true = [0]
-    #     students = SESSION.query(Student).all()
-    #     for Student in students:
-    #         student_name = Student.get_user_by_email 
-    #         if student_name == student_test.name:
-    #             print "Student found: "
-    #             print student_name
-    #             is_true += 1
-    #     expected_results = [1]
 
     def test_all_students_assigned(self):
         """Test to see if all students are ssigned to a project"""
@@ -102,18 +56,14 @@ class BasicTests(unittest.TestCase):
             else: 
                 print "Students Assigned"
     
-#TODO: Student is in the session it was assigned to 
     def test_is_student_in_session(self):
         """Tests to see if a student is in the session that it is assigned to"""
-        #Go through each session and check to see if any session in that session number
-        #has that student in it.
         students = SESSION.query(Student).all()
         sess1 = SESSION.query(Project).filter_by(session_number=1).all()
         sess2 = SESSION.query(Project).filter_by(session_number=2).all()
         sess3 = SESSION.query(Project).filter_by(session_number=3).all()
         sess4 = SESSION.query(Project).filter_by(session_number=4).all()
         for student in students:
-            #how to get specific session? 
             for project in sess1:
                 print "yes"
                 has_student = 0
